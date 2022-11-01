f1c58f459348289af1133c34a49d67bd0a47b08f
Tyfeeds <jayeyforeign07@gmail.com>
2022-11-01 18:29:19 +0100

    landing page, ride with transeat, book ride with transeat, choose vehicle and seat. passenger dashboard not fixed yet

--- main/js/sign-in.js
+++ main/js/sign-in.js
@@ -1,22 +1,22 @@
-const mail= document.getElementById('emailJs')
-const password = document.getElementById('passwordJs')
-const form = document.getElementById('formJs')
-const errorElement = document.getElementById('signInError')
+// const mail= document.getElementById('emailJs')
+// const password = document.getElementById('passwordJs')
+// const form = document.getElementById('formJs')
+// const errorElement = document.getElementById('signInError')
 
-form.addEventListener("submit", (e) => {
-  let messages = [] 
-  if (mail.value === '' || mail.value == null) {
-    messages.push('username or email is required')
-  }
+// form.addEventListener("submit", (e) => {
+//   let messages = [] 
+//   if (mail.value === '' || mail.value == null) {
+//     messages.push('username or email is required')
+//   }
 
-  if (password.value.length <= 6) {
-    messages.push ('password can not be lesser than six characters')
-  }
+//   if (password.value.length <= 6) {
+//     messages.push ('password can not be lesser than six characters')
+//   }
 
  
-  if (messages.length > 0) {
-    e.preventDefault()
-   errorElement.innerText = messages.join(' ,  ')
-  }
-})
+//   if (messages.length > 0) {
+//     e.preventDefault()
+//    errorElement.innerText = messages.join(' ,  ')
+//   }
+// })
 
